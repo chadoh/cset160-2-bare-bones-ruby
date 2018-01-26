1d64dd429dc21bcd703419e2603adb8bdaf67e6f
Chad Ostrowski <hi@chadoh.com>
2018-01-25 21:34:08 -0500

    Add some simple routing

--- app.rb
+++ app.rb
@@ -1,13 +1,20 @@
 require 'rack'
 
 app = Proc.new do |env|
+  path = env['PATH_INFO'][1..-1]
+  filename = if path == ""
+               "index"
+             elsif File.file?("views/#{path}.html")
+               path
+             else
+               "404"
+             end
+  page = File.read("views/#{filename}.html")
+
   [
     '200',
     {'Content-Type' => 'text/html'},
-    ["
-      <h1>Hello world!</h1>
-      <p>The time is #{Time.now}</p>
-    "]
+    [page]
   ]
 end
 
