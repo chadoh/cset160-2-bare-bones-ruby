617a692d5debf4f793179993e50f7cbb355ff264
Chad Ostrowski <hi@chadoh.com>
2018-01-28 15:34:59 -0500

    Use ActiveRecord instead of home-grown db interface

--- models/student.rb
+++ models/student.rb
@@ -1,40 +1,9 @@
-require "sqlite3"
-require "ostruct"
+require "active_record"
 
-class Student
-  DB = SQLite3::Database.new "./db/dev.db"
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/dev.db",
+)
 
-  def self.all
-    student_array = DB.execute("SELECT * FROM students")
-    student_array.map do |attrs|
-      new(*attrs)
-    end
-  end
-
-  def self.create(params)
-    DB.execute(
-      "INSERT INTO students (#{params.keys.join(',')}) VALUES (?, ?, ?)",
-      params.values
-    )
-  end
-
-  def self.find(id)
-    attrs = DB.execute("SELECT * FROM students WHERE id=?", id).first
-    new(*attrs)
-  end
-
-  attr_reader :id, :name, :email, :discord
-
-  def initialize(id, name, email, discord)
-    @id = id
-    @name = name
-    @email = email
-    @discord = discord
-  end
-
-  def delete
-    DB.execute(
-      "DELETE FROM students WHERE id=#{id}"
-    )
-  end
+class Student < ActiveRecord::Base
 end
